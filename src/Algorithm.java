453416ac39aedf8666032e1aa7c36fd65846e886
Yaoyuan Liu <lyy910619@gmail.com>
2016-09-26 10:58:21 -0400

    Rebase merger for Algorithm.java

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,9 +1,6 @@
 import java.util.Set;
 
-/**
- * Created by yaoyuanliu on 9/26/16.
- */
 public interface Algorithm {
-    public Site findBestStartingPoint(Set<Site> unvisitedSites);
-    public Site findNextSite(int currentTime, Set<Site> unvisitedSites, Site currentSite, int currentDay);
+    Site findBestStartingPoint(Set<Site> unvisitedSites);
+    Site findNextSite(int currentTime, Set<Site> unvisitedSites, Site currentSite, int currentDay);
 }
